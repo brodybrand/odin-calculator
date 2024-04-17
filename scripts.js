00ddd13a7208232377679aad3f10d33229650564
Brody Brand <80013712+brodybrand@users.noreply.github.com>
2024-04-17 10:08:57 -0700

    feat: add functionality to two variable operations
    Basic calculations using two numbers and an
    operator now possible (+ - x /)

--- scripts.js
+++ scripts.js
@@ -1,60 +1,76 @@
-function add(a, b) {
-    return a + b;
-}
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-function operate(a, operator, b) {
-    return operator(a, b);
-}
-
 function clearDisplay() {
-    return display.textContent = 0;
+    display.textContent = 0;
 }
 
-let numberA = 0;
-let numberB = 0;
+function clearVariables() {
+    numberA = '';
+    numberB = '';
+    operator = '';
+}
+
+const mathFunctions = {
+    add: function(a, b) {return a + b},
+    subtract: function(a, b) {return a - b},
+    multiply: function(a, b) {return a * b},
+    divide: function(a, b) {return a / b},
+}
+
+let numberA = '';
+let numberB = '';
 let operator = '';
 
 let display = document.querySelector("#display")
 
 const numberButtons = document.querySelectorAll(".number");
-numberButtons.forEach(numberButton => {
-    numberButton.addEventListener("click", () => {
-        if (operator = '') {
-            numberA += numberButton.textContent;
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (operator === '') {
+            if (display.textContent === '0') {
+                numberA = button.textContent;
+                display.textContent = button.textContent;
+            } else {
+                numberA += button.textContent;
+                display.textContent += button.textContent;
+            }
         } else {
-            numberB += numberButton.textContent;
+            numberB += button.textContent;
+            display.textContent += button.textContent;
         }
-        display.textContent += numberButton.textContent;
     })
 })
 
 const operatorButtons = document.querySelectorAll(".operator");
-operatorButtons.forEach(operatorButton => {
-    operatorButton.addEventListener("click", () => {
-        display.textContent += operatorButton.textContent;
-        operator = operatorButton.textContent;
-        console.log(operatorButton.id)
+operatorButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        display.textContent += button.textContent;
+        if (button.id === "add") {operator = mathFunctions.add};
+        if (button.id === "subtract") {operator = mathFunctions.subtract};
+        if (button.id === "multiply") {operator = mathFunctions.multiply};
+        if (button.id === "divide") {operator = mathFunctions.divide};
     })
 })
 
 const enterButton = document.querySelector("#enter");
 enterButton.addEventListener("click", () => {
-    display.textContent = operate(numberA, operator, numberB);
+    display.textContent = operator(+numberA, +numberB);
+    numberA = operator(+numberA, +numberB);
+    numberB = 0;
 })
 
 const clearButton = document.querySelector("#clear");
 clearButton.addEventListener("click", () => {
     clearDisplay();
+    clearVariables();
+})
+
+const allButtons = document.querySelectorAll("button");
+allButtons.forEach(button => {
+    button.addEventListener("mousedown", () => {
+        button.style.border = 'solid black 2px';
+        button.style.fontSize = 'medium';
+    })
+    button.addEventListener("mouseup", () => {
+        button.style.border = 'solid grey 2px';
+        button.style.fontSize = 'small';
+    })
 })
